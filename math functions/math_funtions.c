42121ef189a44454ffe10846708e144ee5c99455
Karnesh Kumar S <karnesh1919@gmail.com>
2026-04-18 19:07:58 +0530

    got to know math functions

---
+++ math functions/math_funtions.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    //int x = 2;
+    float x = 3.14;
+
+
+
+    //x = sqrt(x);
+    //x = round(x);
+    //x = ceil(x);  round down
+    //x = floor(x); round up
+    //x = abs(x);
+    //x = log(x);
+    //x = sin(x);
+    //x = cos(x);
+    //x = tan(x);
+
+    printf("%f",x);
+
+
+
+return 0;
+}
---
+++ math functions/tempCodeRunnerFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    //int x = 2;
+    float x = 3.14;
+
+
+
+    //x = sqrt(x);
+    x = round(x);
+
+
+    printf("%f",x);
+
+
+
+return 0;
+}
